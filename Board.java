214b304122c7fca67b87aa9e2d01de1c5948dff7
Alessandra-Mac <contact@alessandramacleod.com>
2024-12-19 22:09:42 +1300

    GameMain Initialized Missing GameState Enum created Frame created Bug for game logic checking within columns fixed Game logic now works as intended and has been tested.

--- Board.java
+++ Board.java
@@ -5,10 +5,10 @@ public class Board {
 	public static final int GRID_WIDTH = 8;
 	// grid line half width
 	public static final int GRID_WIDTH_HALF = GRID_WIDTH / 2;
-	
+
 	//2D array of ROWS-by-COLS Cell instances
 	Cell [][] cells;
-	
+
 	/** Constructor to create the game board */
 	public Board() {
 	cells = new Cell[GameMain.ROWS][GameMain.COLS]; //Initialise the cells array using ROWS and COLS constants
@@ -18,9 +18,9 @@ public class Board {
 			}
 		}
 	}
-	
 
-	 /** Return true if it is a draw (i.e., no more EMPTY cells) */ 
+
+	 /** Return true if it is a draw (i.e., no more EMPTY cells) */
 	public boolean isDraw() {
 		 boolean drawCheck = false;
 		 for (int row = 0; row < GameMain.ROWS; row++) {
@@ -31,22 +31,23 @@ public class Board {
 		 }
 	return true; //Game has reached a draw
 	}
-	
+
 	/** Return true if the current player "thePlayer" has won after making their move  */
 	public boolean hasWon(Player thePlayer, int playerRow, int playerCol) {
 
 		// check if player has 3-in-that-row
-		if (cells[playerRow][0].content == thePlayer &&
+		if (
+				cells[playerRow][0].content == thePlayer &&
 				cells[playerRow][1].content == thePlayer &&
-				cells[playerRow][2].content == thePlayer) {
-			return true; // Player Win
-		}
+				cells[playerRow][2].content == thePlayer)
+			{return true;} // Player Win
+
 
 		//Check if the player has 3 in the playerCol.
 		if (
-				cells[playerCol][0].content == thePlayer &&
-				cells[playerCol][1].content == thePlayer &&
-				cells[playerCol][2].content == thePlayer)
+				cells[0][playerCol].content == thePlayer &&
+				cells[1][playerCol].content == thePlayer &&
+				cells[2][playerCol].content == thePlayer)
 		{return true;} // Player Win
 
 		 // 3-in-the-diagonal
@@ -64,7 +65,7 @@ public class Board {
 		{return true;} // Player Win
 		return false; //no winner, keep playing
 	}
-	
+
 	/**
 	 * Draws the grid (rows then columns) using constant sizes, then call on the
 	 * Cells to paint themselves into the grid
@@ -74,24 +75,24 @@ public class Board {
 		g.setColor(Color.gray);
 
 
-		for (int row = 1; row < GameMain.ROWS; ++row) {          
+		for (int row = 1; row < GameMain.ROWS; ++row) {
 			g.fillRoundRect(0, GameMain.CELL_SIZE * row - GRID_WIDTH_HALF,
-					GameMain.CANVAS_WIDTH - 1, GRID_WIDTH,                
-					GRID_WIDTH, GRID_WIDTH);       
+					GameMain.CANVAS_WIDTH - 1, GRID_WIDTH,
+					GRID_WIDTH, GRID_WIDTH);
 			}
-		for (int col = 1; col < GameMain.COLS; ++col) {          
+		for (int col = 1; col < GameMain.COLS; ++col) {
 			g.fillRoundRect(GameMain.CELL_SIZE * col - GRID_WIDTH_HALF, 0,
-					GRID_WIDTH, GameMain.CANVAS_HEIGHT - 1,                
+					GRID_WIDTH, GameMain.CANVAS_HEIGHT - 1,
 					GRID_WIDTH, GRID_WIDTH);
 		}
-		
+
 		//Draw the cells
-		for (int row = 0; row < GameMain.ROWS; ++row) {          
-			for (int col = 0; col < GameMain.COLS; ++col) {  
+		for (int row = 0; row < GameMain.ROWS; ++row) {
+			for (int col = 0; col < GameMain.COLS; ++col) {
 				cells[row][col].paint(g);
 			}
 		}
 	}
-	
+
 
 }
---
+++ GameState.java
@@ -0,0 +1,9 @@
+public enum GameState {
+    /**
+     * Enumeration for the Game State
+     */
+    Playing,
+    Draw,
+    Cross_won,
+    Nought_won
+}
